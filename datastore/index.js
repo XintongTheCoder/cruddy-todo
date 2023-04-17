2a3db9e6b67cf493d603d2a7815b18bf0c3fd671
XintongTheCoder <xintongmi@gmail.com>
2023-04-17 16:37:03 -0700

    complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -99,14 +99,28 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
+  fs.exists(path.join(exports.dataDir, `${id}.txt`), (exists) => {
+    if (!exists) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+        if (err) {
+          callback(new Error('Failed to delete file'));
+        } else {
+          console.log('Deleted!');
+          callback();
+        }
+      });
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
